179eaa7b7812f8d802af0cc1f81521cab369e161
Tatyana <hanyatan@gmail.com>
2020-10-06 11:49:29 -0400

    styling

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -11,12 +11,7 @@ import Footer from "../components/Footer";
 function Books() {
   const [search, setSearch] = useState("");
   const [books, setBooks] = useState([])
-  const [id, setId] = useState("");
-  const [title, setTitle] = useState("");
-  const [link, setLink] = useState("");
-  const [image, setImage] = useState("");
-  const [author, setAuthor] = useState("");
-  const [description, setDescription] = useState("");
+
   const [error, setError] = useState("");
 
   useEffect(() => {
@@ -25,45 +20,29 @@ function Books() {
     }
 
     API.fetchBooks(search)
-    .then(res => {
-      const booksArr = res.data;
-      console.log(booksArr);
-      console.log(search);
-      setBooks(booksArr);
-      // setBooks(booksArr.map(book => {
-      //   return {
-      //     _id: book.id,
-      //     title: book.volumeInfo.title,
-      //     image: book.volumeInfo.imageLinks.thumbnail,
-      //     authors: book.volumeInfo.authors,
-      //     link: book.volumeInfo.previewLink,
-      //     description: book.volumeInfo.description,
-      //   }
-      // }));
-      if (res.data.length === 0) {
-        throw new Error("No results found.");
-      }
-      if (res.data.status === "error") {
-        throw new Error(res.data.message);
-      }
-      // setId(booksArr[0][1]);
-      // setTitle(booksArr[7][20]);
-      // setLink(booksArr[7][0][9]);
-      // setImage(booksArr[7][7][1]);
-      // setAuthor(booksArr[7][1]);
-      // setDescription(booksArr[7][6]);
-    })
-    .catch(err => setError(err));
+      .then(res => 
+
+        setBooks(res.data)
+
+        // if (res.data.length === 0) {
+        //   throw new Error("No results found.");
+        // }
+        // if (res.data.status === "error") {
+        //   throw new Error(res.data.message);
+        // }
+      )
+      .catch(err => setError(err));
   }, [search]);
 
   const handleInputChange = event => {
     setSearch(event.target.value);
   };
   const handleFormSubmit = event => {
+
+    event.preventDefault();
     console.log('Submitted');
     console.log(books);
-    event.preventDefault();
-    setSearch(event.target.value);
+    console.log(books.length);
   };
 
   return (
@@ -71,41 +50,41 @@ function Books() {
       <Row>
         <Col size="md-12">
           <Jumbotron>
-          <i className="fas fa-book fa-3x" ></i>
+            <i className="fas fa-book fa-3x" ></i>
             <h1>(React) Google Book Search</h1>
             <p>Search for and Save Books of Interest</p>
             <SearchForm
-            handleInputChange={handleInputChange}
-            onSubmit={handleFormSubmit}
-            results={search}
+              handleInputChange={handleInputChange}
+              handleFormSubmit={handleFormSubmit}
+              results={search}
             />
-            <br/>
-            <br/>
+            <br />
+            <br />
           </Jumbotron>
-          
+
           <Jumbotron>
             <h2>Results</h2>
-            {books.length ? (
-              <List>
-                {books.map(book => (
-                  <ListItem key={book.id}>
-                    <Link to={"/books/" + book._id}>
-                      <strong>
-                        {book.volumeInfo.title} by {book.volumeInfo.authors}
-                      </strong>
-                    </Link>
-                    {/* <DeleteBtn onClick={() => deleteBook(book._id)} /> */}
-                  </ListItem>
-                ))}
-              </List>
-            ) : (
+
+          </Jumbotron>
+          {books.length ? (
+            <List>
+              {books.map(book => (
+                <ListItem key={book.id}>
+                  <Link to={"/books/" + book._id}>
+                    <strong>
+                      {book.volumeInfo.title} by {book.volumeInfo.authors}
+                    </strong>
+                  </Link>
+                  {/* <DeleteBtn onClick={() => deleteBook(book._id)} /> */}
+                </ListItem>
+              ))}
+            </List>
+          ) : (
               <h3>No Results to Display</h3>
             )}
-          </Jumbotron>
-          
         </Col>
       </Row>
-      <Footer/>
+      <Footer />
     </Container>
   );
 }
